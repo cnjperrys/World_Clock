5072a356b024ae5e5278390e241bdfeca868d820
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-02-21 23:56:54 -0500

    update html & JS

--- JS/index.js
+++ JS/index.js
@@ -19,11 +19,22 @@ sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>]A[</small>]");
 }
 
 
-updateCity(event) {
+function updateCity(event) {
     let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = cityTimeZone;
+    citiesElement.innerHTML =`
+   
+    <div class="city">
+        <div>
+        <h2>${cityTimeZone}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+             <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div> 
+        </div>
+    `;
+
 }
 
 
@@ -35,4 +46,4 @@ setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 
-citiesSelectElement.addEventListener("change", updaetCity);
+citiesSelectElement.addEventListener("change", updateCity);
