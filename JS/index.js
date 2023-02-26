5819200290c6b5b7a1f2e49d1de963c3826dd5a2
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-02-26 13:52:50 -0500

    Added time zones

--- JS/index.js
+++ JS/index.js
@@ -6,7 +6,7 @@ let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss[<small>]A[</small>]");
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss[<small>] A[</small>]");
 
 
 
@@ -15,7 +15,7 @@ let sydneyDateElement = sydneyElement.querySelector(".date");
 let sydneyTimeElement = sydneyElement.querySelector(".time");
 let sydneyTime = moment().tz("Australia/Sydney");
 sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
-sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>]A[</small>]");
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>] A[</small>]");
 }
 
 
@@ -24,18 +24,17 @@ function updateCity(event) {
     if (cityTimeZone ==="current") {
         cityTimeZone = moment.tz.guess();
     }
-    cityTimeZone = cityTimeZone.replace("_", " ").split("/")[1];
-    cityTimeZone = cityTimeZone.replace("/", " ");
+    cityName= cityTimeZone.replace("/", " ");
+    cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = cityTimeZone;
-    citiesElement=`
+    citiesElement.innerHTML = `
     <div class="city">
         <div>
-        <h2>${cityTimeZone}</h2>
+        <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-             <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div> 
+             <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(" A")}</small></div> 
         </div>
         <a href="/">back to all cities</a>
     `;
